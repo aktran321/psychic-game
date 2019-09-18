f79b60e7b51408f94b2b839b1666014f7f23889b
Andy Tran <aktran321@gmail.com>
2019-09-18 18:37:28 -0400

    the game seems to be working perfectly. CSS not added yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,33 +7,63 @@ var ties = 0;
 var lives = 9;
 
 var userText = document.getElementById("user-text");
-var computerText = document.getElementById("computer-text");
-var tiesText = document.getElementById("ties-text");
-var lossText = document.getElementById("losses-text");
+var lossesText = document.getElementById("losses-text");
 var winsText = document.getElementById("wins-text");
+var livesText = document.getElementById("lives-text");
+var pastGuesses = document.getElementById("guesses-text");
 
 var computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-              console.log(computerGuess);
-              computerText.textContent=computerGuess;
-              console.log(computerOptions.indexOf(computerGuess));
+            console.log(computerGuess);
+            console.log(computerOptions.indexOf(computerGuess));
 
 document.onkeyup=function(event){
     var userGuess = event.key;
-    
- 
+    userText.textContent=userGuess;
+
+    pastGuesses.textContent += " " + userGuess;
     // This is just saying if the 
     if(computerOptions.indexOf(userGuess) == computerOptions.indexOf(computerGuess)){
         console.log("you are correct!")
-       
-        //we also want wins to go up and to reset the game
-        //guesses to print into the html
-        userText.textContent=userGuess;
+        wins++;
+        winsText.textContent= wins;
+        // now reset the lives and the computers guess
+        computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+            console.log("New Random Letter" + " "+ computerGuess);
+            console.log(computerOptions.indexOf(computerGuess));
+        //resetting the lives
+        lives=9;
+        //printing for the user to see
+        livesText.textContent= lives;
+        // Resetting the pastGuesses
+        pastGuesses.textContent = "";
+        
     } 
     else{
+        console.log("WRONG!");
         // we want lives to go down
-        // 
-        console.log("you are a dumby");
-        userText.textContent=userGuess;
+        lives--;
+        livesText.textContent= lives;
+        console.log(lives);
+        
+        
+        
         // Once lives integer = 0, we do losses ++ and reset the game
+    }
+    if(lives==0){
+        //RESET THE GAME: losses would go up by 1, the guesses history will clear, and the lives left will go back to 9
+        losses++;
+        console.log(losses);
+        // print out the updated losses
+        lossesText.textContent = losses;
+        lives=9;
+        // once lives gets reset, we want to make sure we update the value so it appears on the screen
+        livesText.textContent= lives;
+        // now we want to reset the computers guess
+        computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+            console.log("New Random Letter" + " "+ computerGuess);
+            console.log(computerOptions.indexOf(computerGuess));
+         // Resetting the pastGuesses
+         pastGuesses.textContent = "";
+
     }
 }
